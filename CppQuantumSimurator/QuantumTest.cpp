0ad0325bde42c533919381acadf396653bda7d01
chemical <s.kojima.8263.allez@gmail.com>
2022-12-15 15:20:17 +0900

    Commit for Sync(221215)

--- CppQuantumSimurator/QuantumTest.cpp
+++ CppQuantumSimurator/QuantumTest.cpp
@@ -4,6 +4,7 @@ using namespace QuantumSystem;
 
 int main()
 {
+    /*
     QuantumCircuit qc;
     qc.Init(2);
 
@@ -12,6 +13,10 @@ int main()
 
     qc.Detection(0);
     qc.Detection(1);
-
+    */
+    float a = 0.01;
+    float b = 0;
+    for (int i = 0; i < 10000; ++i) b += a;
+    std::cout << b << std::endl;
     return 0;
 }

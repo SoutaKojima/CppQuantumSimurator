f3305159aef7819994023868049c3d428fc232ff
chemical <s.kojima.8263.allez@gmail.com>
2023-01-21 23:55:32 +0900

    Generate sin_curve.dat(2revised)

--- CppQuantumSimurator/QuantumMain.cpp
+++ CppQuantumSimurator/QuantumMain.cpp
@@ -14,7 +14,7 @@ int main()
 	for (int i = 0; i < 100; ++i) {
 		double x = distr(eng) * 2 - 1;
 		text_data << x << ' ';
-		double noise = (distr(eng) * 2 - 1) * 0.015;
+		double noise = (distr(eng) * 2 - 1) * 0.05;
 		double f_x = sin(x*_Pi) + noise;
 		text_data << f_x << endl;
 	}
